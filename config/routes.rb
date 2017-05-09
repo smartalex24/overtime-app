3b840a2c37ad91b175235a0c241a1ce0fdd8174e
Alex Cortes <alexcortes247@gmail.com>
2017-05-09 15:25:32 -0600

    Added rspec, capybara and setup homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # config/routes.rb
+  root to: 'static#homepage'
 end
+
