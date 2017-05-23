40b9613b70731bb876715669400e967d301fea78
Alex Cortes <alexcortes247@gmail.com>
2017-05-23 15:58:08 -0600

    Updated seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,8 @@
                     password: "asdfasdf",
                     password_confirmation: "asdfasdf",
                     first_name: "Jon",
-                    last_name: "Snow")
+                    last_name: "Snow",
+                    phone: "4422426120")
 
 puts "1 User created"
 
@@ -22,7 +23,7 @@ end
 puts "100 Posts have been created"
 
 100.times do |audit_log|
-  AuditLog.create!(user_id: User.last.id, status: 0, start_date: (Date.today - 6.days))
+  AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 6.days))
 end
 
 puts "100 audit logs have been created"
